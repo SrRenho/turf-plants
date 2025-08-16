ffbcfd57eeb02f365aae88261e85a3b776e2db3f
SrRenho <juanjogvizioli@gmail.com>
2025-08-16 00:24:48 -0300

    remove zombie code

--- backend/turf_plants/urls.py
+++ backend/turf_plants/urls.py
@@ -15,16 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('accounts/', include('allauth.urls')),                   # Google OAuth
-
-    # game API
     path('game_api/', include('game_api.urls')),
-
-  #  re_path(r'^.*$', TemplateView.as_view(template_name="index.html")),
 ]
