b2b3cc319bc1bf7bad7d0575db6744403f7c8693
SrRenho <juanjogvizioli@gmail.com>
2025-08-16 02:15:36 -0300

    esto me pasa por no usar un ide con syntax highlight

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { BACKEND, WS_BACKEND } from './config';
 
 export default function Game(){
